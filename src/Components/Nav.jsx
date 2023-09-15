069254f044bfea22b7509c6b6bc6a6d94ebc796f
AmineBENCHOUCHE <amine.benchouche@gmail.com>
2023-09-15 10:31:43 +0200

    Import react icons and add close ison on the menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,6 +2,7 @@ import {headerLogo} from "../assets/images"
 import{hamburger} from "../assets/icons"
 import { navLinks } from "../constants"
 import { useState } from "react"
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 
 const links =  () => (
@@ -20,9 +21,9 @@ const links =  () => (
    const [toggleMenu, setToggleMenu] = useState(false);
    return (
     <header className="padding-x py-8 absolute z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
+        <nav className="flex justify-between max-container">
             <a href="/">
-                <img src={headerLogo} alt="header logo" width={130} height={29}/>
+                <img src={headerLogo} alt="header logo" width={130} height={29} />
             </a>
             <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
             {links()}
@@ -31,17 +32,14 @@ const links =  () => (
               <div className="flex flex-col justify-center items-end">
                 {
                   toggleMenu? 
-                  <img id="menuIcon" src={hamburger} alt="hamburger menu" width={25} height={25} onClick={() => setToggleMenu(false)} />:
-                  <img  src={hamburger} alt="hamburger menu" width={25} height={25} onClick={() => setToggleMenu(true)} />
-
+                  <RiCloseLine alt="close menu" width={27} height={27} onClick={() => setToggleMenu(false)} />:
+                  <RiMenu3Line alt="hamburger menu" width={27} height={27} onClick={() => setToggleMenu(true)} />
                 }
                 {
                   toggleMenu && (
-
-                    <div className="flex flex-col justify-end items-end list-none relative mt-5 mr-5">
-                   
+                   <div className="flex flex-col justify-end items-end list-none relative mt-5 mr-5">
                     {links()}
-                    </div>
+                   </div>
                   )
                   
                 }
