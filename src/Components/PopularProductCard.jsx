9e8877ffcf90a6c59215d1f1a4e6f8e5f9e24630
AmineBENCHOUCHE <amine.benchouche@gmail.com>
2023-09-19 14:34:26 +0200

    Add Popular Product section

--- src/Components/PopularProductCard.jsx
+++ src/Components/PopularProductCard.jsx
@@ -3,16 +3,15 @@ import { star } from '../assets/icons'
 
 const PopularProductCard = ({imgURL, name, price}) => {
   return (
-    <div className='flex flex-1 flex-col w-full max-sm:w-full shadow-md shadow-red-300  p-4 rounded-lg'>
+    <div className='flex flex-1 flex-col w-full max-sm:w-full shadow-md shadow-red-300  p-4 rounded-lg '>
         <img src={imgURL} alt={name} className='w-full h-auto ' />
         <div className='mt-8 flex flex-col justify-start gap-2.5'>
             <div className='flex gap-2'>
                 <img src={star} alt="rating"  width={24} height={24} className="color-red-500 "/>
                 <p className='font-montserrat leading-normal text-slate-gray text-xl'>(4.8)</p>
             </div>
-            <h3 className='text-xl mt-2 leading-normal font-semibold font-palanquin '>{name}</h3>
-            <p className='mt-2 font-montserrat font-semibold text-2xl leading-normal text-red-500'>{price}</p>
-
+            <h3 className='text-2xl mt-2 leading-normal font-semibold font-palanquin '>{name}</h3>
+            <p className=' relative  mt-2 font-montserrat font-semibold text-2xl leading-normal text-red-500 '>{price}</p>
         </div>
 
     </div>
