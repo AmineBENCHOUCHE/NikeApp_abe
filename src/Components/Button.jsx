0dd231424bd81d12e055ad141a0926fe28c58861
AmineBENCHOUCHE <amine.benchouche@gmail.com>
2023-09-21 10:47:57 +0200

    Update Button props

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { IconBasketBall } from '../assets/images';
 
-const Button = ({label, iconURL, backgroundColor, textColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, fullWidth}) => {
   const [iconImage, setIconImage] = useState(iconURL);
   const handleClickBtn = () => {
 
@@ -12,12 +12,16 @@ const Button = ({label, iconURL, backgroundColor, textColor}) => {
 }
   return (
     <button 
-      className={`flex justify-center items-center gap-2 font-montserrat border-2  border-red-300 text-lg leading-none px-7 py-4 rounded-full
+      className={`flex justify-center items-center gap-2 font-montserrat hover:bg-red-400 border-2 border-red-300 text-lg leading-none px-7 py-4 rounded-full
                   ${ 
                     backgroundColor? 
                     `${backgroundColor} ${textColor}` :
-                     "bg-red-500 text-white "
-                  }}
+                     "bg-red-500 text-white " 
+                  }
+                  ${
+                    fullWidth && 'w-full'
+                  }
+                }
                 `}
      onClick={handleClickBtn}>
         {label}
